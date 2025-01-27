8f0fb852694b31e3e6c0d521699b73df31755900
bordasd <bordasd@201-05>
2025-01-27 08:50:50 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace eredmenyek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Versenyzo> versenyzok = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbxVersenyzok.Items.Clear();
+            string tartomany = "Eredmenyek.txt";
+            Feltolt(tartomany);
+
+            lbxVersenyzok.ItemsSource = versenyzok;
+            //lbxVersenyzok.DisplayMemberPath = "Nev"; 
+        }
+
+        public void Feltolt(string tartomany)
+        {
+            try
+            {
+                string[] bemenet = File.ReadAllLines(tartomany);
+                foreach (string sor in bemenet)
+                {
+                    string[] adatok = sor.Split(';');
+                    versenyzok.Add(new Versenyzo()
+                    {
+                        Sorszaszam = int.Parse(adatok[0]),
+                        Nev = adatok[1],
+                        Feladat1 = int.Parse(adatok[2]),
+                        Feladat2 = int.Parse(adatok[3]),
+                        Feladat3 = int.Parse(adatok[4])
+                    });
+                }
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Hiba: " + e.Message);
+            }
+        }
+
+        private void lbxVersenyzok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tbxSorszam.Text = (lbxVersenyzok.SelectedItem as Versenyzo).Sorszaszam.ToString();
+            tbxNev.Text = (lbxVersenyzok.SelectedItem as Versenyzo).Nev;
+            tbx1feladat.Text = (lbxVersenyzok.SelectedItem as Versenyzo).Feladat1.ToString();
+            tbx2feladat.Text = (lbxVersenyzok.SelectedItem as Versenyzo).Feladat2.ToString();
+            tbx3feladat.Text = (lbxVersenyzok.SelectedItem as Versenyzo).Feladat3.ToString();
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eredmenyek
+{
+    class Versenyzo
+    {
+        // Olyan osztályt amiben csak propery-k vannak, rekordnak hívjuk.
+        public int Sorszaszam { get; set; }
+        public string Nev { get; set; }
+        public int Feladat1 { get; set; }
+        public int Feladat2 { get; set; }
+        public int Feladat3 { get; set; }
+
+
+        public Versenyzo() { }
+
+
+        public Versenyzo(int sorszaszam, string nev, int feladat1, int feladat2, int feladat3)
+        {
+            Sorszaszam = sorszaszam;
+            Nev = nev;
+            Feladat1 = feladat1;
+            Feladat2 = feladat2;
+            Feladat3 = feladat3;
+        }
+
+        //public Versenyzo(string sor)
+        //{
+        //    string[] adatok = sor.Split(';');
+        //    Sorszaszam = int.Parse(adatok[0]);
+        //    Nev = adatok[1];
+        //    Feladat1 = int.Parse(adatok[2]);
+        //    Feladat2 = int.Parse(adatok[3]);
+        //    Feladat3 = int.Parse(adatok[4]);
+        //}
+
+        public override string ToString()
+        {
+            return Nev;
+        }
+    }
+}
